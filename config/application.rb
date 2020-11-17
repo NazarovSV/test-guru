77c6db7ed2eb3aeb3db00e979e6aa22bc951c79e
NazarovSV <badger.lock@yandex.ru>
2020-11-17 21:41:59 +0300

    Правки по результатам ревью

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,10 @@ module TestGuru
     config.i18n.default_locale = :ru
     config.i18n.locale = :ru
 
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
   end
 end
