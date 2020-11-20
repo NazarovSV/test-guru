874ff7177b83c9e72ec3d3c576822e2bcdaec1a4
NazarovSV <badger.lock@yandex.ru>
2020-11-20 13:18:15 +0300

    Для Users завел запрет на null для полей email и name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_100800) do
+ActiveRecord::Schema.define(version: 2020_11_20_101601) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
