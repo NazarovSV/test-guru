da1527d9373bda09956e47782d28c99aafa232fc
NazarovSV <badger.lock@yandex.ru>
2020-11-20 14:38:36 +0300

    Добавлена модель Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_102142) do
+ActiveRecord::Schema.define(version: 2020_11_20_113647) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_11_20_102142) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tests", "categories"
 end
