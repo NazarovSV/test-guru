2e0b6d488d8fe6e37c463b6138cd2792fe2e51d9
NazarovSV <badger.lock@yandex.ru>
2020-11-23 14:38:03 +0300

    Добавил инстанс метод в User, отображающий все тесты, которые прошел пользователь

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def completed_tests_by_level(level)
+    user_completed_test_ids = UserProgress.where(user_id: self.id).pluck(:test_id)
+    Test.where(level: level, id: user_completed_test_ids)
+  end
+
 end
