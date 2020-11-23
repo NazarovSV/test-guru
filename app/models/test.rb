6b3ffa4840685b24461138daa2f5ebfa99ce9da0
NazarovSV <badger.lock@yandex.ru>
2020-11-23 14:37:21 +0300

    В Test добавлен метод, позволяющий по имени категории получить отсортированный по убыванию массив имен тестов

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.test_names_by_category_name_sorted_desc(category_name)
+    category_ids = Category.where(title: category_name)
+    Test.where(category_id: category_ids).order('title ASC').pluck(:title)
+  end
+
 end
