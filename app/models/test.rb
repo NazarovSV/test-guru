27137044fcaf024d03eea69bfa596a9e1c221610
NazarovSV <badger.lock@yandex.ru>
2020-11-23 16:46:54 +0300

    Упростил test_names_by_category_name_sorted_desc в Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,10 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.test_names_by_category_name_sorted_desc(category_name)
-    category_ids = Category.where(title: category_name)
-    Test.where(category_id: category_ids).order('title ASC').pluck(:title)
+    joins("JOIN categories ON tests.category_id = categories.id")
+      .where(categories: {title: category_name})
+      .order(title: :desc)
+      .pluck(:title)
   end
 
 end
