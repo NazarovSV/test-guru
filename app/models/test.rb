cceddaf5b55664cf4f6a499a0ba199dbedd22a8f
NazarovSV <badger.lock@yandex.ru>
2020-11-24 18:16:48 +0300

    Добавил ассоциации между моделями

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  belongs_to :user
+  has_many :user_progresses
+  has_many :users, through: :user_progresses
 
   def self.test_names_by_category_name_sorted_desc(category_name)
     joins("JOIN categories ON tests.category_id = categories.id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: "Test"
+  has_many :user_progresses
+  has_many :tests, through: :user_progresses
 
   def completed_tests_by_level(level)
     Test.joins("JOIN user_progresses ON user_progresses.test_id = tests.id")
