f56de495f4bb6d04a4b7e156b9d4ab68f6d9a17d
NazarovSV <badger.lock@yandex.ru>
2020-11-29 15:02:06 +0300

    Переписал в моделе Test метод класса на scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,11 +9,9 @@ class Test < ApplicationRecord
   scope :normal, -> {where(level: 2..4)}
   scope :hard, -> {where(level: 5..Float::INFINITY)}
 
-  def self.test_names_by_category_name_sorted_desc(category_name)
-    joins("JOIN categories ON tests.category_id = categories.id")
-      .where(categories: {title: category_name})
-      .order(title: :desc)
-      .pluck(:title)
-  end
+  scope :test_names_by_category_name_sorted_desc, ->(category_name) {joins("JOIN categories ON tests.category_id = categories.id")
+                                                                       .where(categories: {title: category_name})
+                                                                       .order(title: :desc)
+                                                                       .pluck(:title)}
 
 end
