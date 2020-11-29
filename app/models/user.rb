eb48b81a05ffd50606cec3a8475ec978743da8e3
NazarovSV <badger.lock@yandex.ru>
2020-11-29 15:49:41 +0300

    Добавил валидацию в модель User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :user_progresses, dependent: :destroy
   has_many :tests, through: :user_progresses
 
+  validates :name,:email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   def completed_tests_by_level(level)
     Test.joins("JOIN user_progresses ON user_progresses.test_id = tests.id")
         .where(user_progresses: {user_id: self.id, is_completed: true }, level: level)
