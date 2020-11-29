6d540af3178d5177d60ae0272a49688410a79270
NazarovSV <badger.lock@yandex.ru>
2020-11-29 17:08:32 +0300

    Добавил валидацию Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, dependent: :destroy
+
+  validates :body, :test_id, presence: true
+  validates :body, uniqueness: {scope: :test_id, message: 'Question must be unique for the same test'}
 end
