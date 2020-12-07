a6b00646be94db1a27d8227d2627753bf8d08074
NazarovSV <badger.lock@yandex.ru>
2020-12-07 21:41:44 +0300

    Обновил QuestionsController для просмотра вопросов

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+
+  before_action :find_question_by_id, only: [:show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render json: { questions: Question.all }
+  end
+
+  def show
+    render json: { question: @question }
+  end
+
+  private
+
+  def find_question_by_id
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'question was not found'
+  end
+
 end
