d3f557bbbae5ad987adc2dc6a63abedeeab9a291
NazarovSV <badger.lock@yandex.ru>
2020-12-25 17:25:44 +0300

    Добавил нумерацию вопросов в тесте

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -25,6 +25,10 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def question_number
+    test.questions.order(:id).index(current_question) + 1
+  end
+
   private
 
   def before_validation_set_current_question
@@ -40,10 +44,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-      (correct_answers_count == answer_ids.count)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
