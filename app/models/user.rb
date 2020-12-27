842808f9efbfc51284fb344311585f9d416a9bd1
NazarovSV <badger.lock@yandex.ru>
2020-12-27 11:43:17 +0300

    Авторизация

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'digest/sha1'
 class User < ApplicationRecord
   # has_many :owned_tests, class_name: 'Test', dependent: :restrict_with_exception
   # has_many :user_progresses, dependent: :destroy
@@ -20,5 +20,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(password_string)
+    digest(password_string) == password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
