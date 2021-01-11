67da68348cb08f7eed45e9fe7c0c24fe6cd60018
NazarovSV <badger.lock@yandex.ru>
2021-01-11 20:54:51 +0300

    Сделал выход из системы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,4 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to('Github', "https:/github.com/#{author}/#{repo}", target: '_blank', rel: 'nofollow')
   end
-
-  def flash_message(key)
-    content_tag :p, flash[key], class: "flash #{key}" if flash[key]
-  end
 end
