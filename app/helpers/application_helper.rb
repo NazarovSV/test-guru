d9e6a55d5828f57fdeeea951d27ddc5ef83b002f
NazarovSV <badger.lock@yandex.ru>
2021-01-11 20:14:22 +0300

    Добавил хелпер для flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def current_year
     "Current year: #{Time.current.year}"
   end
@@ -8,4 +7,7 @@ module ApplicationHelper
     link_to('Github', "https:/github.com/#{author}/#{repo}", target: '_blank', rel: 'nofollow')
   end
 
+  def flash_message(key)
+    content_tag :p, flash[key], class: "flash #{key}" if flash[key]
+  end
 end
