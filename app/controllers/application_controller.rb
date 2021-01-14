c0179c9d0e80c6824b9209a05d1a5bb19d899413
NazarovSV <badger.lock@yandex.ru>
2021-01-14 15:00:32 +0300

    флеш сообщение при логине

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  protected
+
+  def after_sign_in_path_for(user)
+    flash[:notice] = "Hi, #{user.first_name} #{user.last_name}"
+    if user.is_a?(Admin)
+      admin_tests_url
+    else
+      super
+    end
+  end
+
   private
 
   def configure_permitted_parameters
