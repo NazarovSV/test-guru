e77ccff9c3c9082e2ac8493aa0d18416a1c17020
NazarovSV <badger.lock@yandex.ru>
2021-01-22 02:34:00 +0300

    Рефакторинг UI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,21 @@ module ApplicationHelper
   def flash_message(type)
     content_tag :p, flash[type], class: "flash #{type}" if flash[type]
   end
+
+  def current_progress(total_question, current_question)
+    ((current_question * 1.0) / total_question) * 100
+  end
+
+  def message_type(type)
+    case type
+    when 'notice' then 'alert-success'
+    when 'alert' then 'alert-danger'
+    else
+      'alert-info'
+    end
+  end
+
+  def answer_background_card(correct)
+    correct ? 'bg-success' : 'bg-danger'
+  end
 end
