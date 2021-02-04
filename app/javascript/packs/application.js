c0addf147a8d6752bea09c76e1039f981c1dcd67
NazarovSV <badger.lock@yandex.ru>
2021-02-04 15:29:58 +0300

    Проверка на совпадаение пароле

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../utilities/sorting")
+require("../utilities/password_matching")
 
 import "bootstrap";
 import "../stylesheets/application";
