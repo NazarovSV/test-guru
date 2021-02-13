eaf5fbcd64075b8909ef0bfe4aa50f86cb71b560
NazarovSV <badger.lock@yandex.ru>
2021-02-13 13:18:48 +0300

    Таблица many-to-many для Баджей и Пользователей

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,8 @@
 class Badge < ApplicationRecord
   RULES = %w[first_try hard_test easy_category].freeze
 
+  has_many :user_badges, dependent: :restrict_with_exception
+
   validates :name, presence: true
   validates :rule_type, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
 
   has_many :test_passages
   has_many :tests, through: :test_passages
+  has_many :badges, through: :user_badges
   has_many :owned_tests, class_name: 'Test', foreign_key: :user_id, dependent: :restrict_with_exception
 
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: :wrong_format }
